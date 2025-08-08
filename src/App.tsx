47575a4dd39d735ff1a21d15f52d7000988d502f
Israel Teixeira <israeltx@gmail.com>
2025-08-08 15:05:38 -0300

    Create a hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import "./global.css"
 import { Button } from "./components/button";
 import styles from "./app.module.css"
 
+function useMessage() {
+  function show() {
+    console.log('Mensagem do meu hook');
+  }
+  return { show }
+}
+
 export function App() {
+  const message = useMessage()
+
   return (
     <div className={styles.container}>
-      <Button name='Adicionar'/>
+      <Button name='Adicionar' onClick={() => message.show()}/>
       <span>0</span>
       <Button name='Remover'/>
     </div>
