9e7bf5487e2e8eb7bb89a599f7ba338d02b5854e
Israel Teixeira <israeltx@gmail.com>
2025-08-08 15:15:44 -0300

    Create hooks on separate files

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,14 @@ import "./global.css"
 import { Button } from "./components/button";
 import styles from "./app.module.css"
 
-function useMessage() {
-  function show() {
-    console.log('Mensagem do meu hook');
-  }
-  return { show }
-}
+import { useMessage } from "./hooks/useMessage";
 
 export function App() {
-  const message = useMessage()
+  const { show } = useMessage({name: 'Israel'})
 
   return (
     <div className={styles.container}>
-      <Button name='Adicionar' onClick={() => message.show()}/>
+      <Button name='Adicionar' onClick={() => show('Mensagem do hook')}/>
       <span>0</span>
       <Button name='Remover'/>
     </div>
---
+++ src/hooks/useMessage.ts
@@ -0,0 +1,10 @@
+type Props = {
+  name: string
+}
+
+export function useMessage({name}: Props) {
+  function show(message: string) {
+    console.log(name, message);
+  }
+  return { show }
+}
