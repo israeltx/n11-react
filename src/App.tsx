968760d0b8c4d118555bd0d0ac4b3c2ca77c73f5
Israel Teixeira <israeltx@gmail.com>
2025-08-08 15:39:19 -0300

    Implement useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import "./global.css"
 import { Button } from "./components/button";
 import styles from "./app.module.css"
+import { useState } from "react";
+import "./global.css"
 
-import { useMessage } from "./hooks/useMessage";
+// import { useMessage } from "./hooks/useMessage";
 
 export function App() {
-  const { show } = useMessage({name: 'Israel'})
+  const [count, setCount] = useState(0)
+  // const { show } = useMessage({name: 'Israel'})
 
   return (
     <div className={styles.container}>
-      <Button name='Adicionar' onClick={() => show('Mensagem do hook')}/>
-      <span>0</span>
-      <Button name='Remover'/>
+      <Button name='Adicionar' onClick={() => setCount(count + 1)}/>
+      <span>{count}</span>
+      <Button name='Remover' onClick={() => setCount(count - 1)}/>
     </div>
   )
 }
